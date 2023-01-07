5871c621cfd5c836fb949c12f3c98789370a9e54
Kyle Lawrence <kyle@designanddevelop.io>
2023-01-07 16:02:05 -0600

    Make psr-4 compliant

--- src/Services/GridPaneService.php
+++ src/Services/GridPaneService.php
@@ -2,20 +2,23 @@
 
 namespace KyleAtDND\GridPane\Services;
 
-use Config, InvalidArgumentException, BadMethodCallException;
-use GridPane\API\HttpClient;
+use BadMethodCallException;
+use Config;
+use GridPane\Api\HttpClient;
+use InvalidArgumentException;
 
-class GridPaneService {
-
+class GridPaneService
+{
     /**
      * Get auth parameters from config, fail if any are missing.
      * Instantiate API client and set auth bearer token.
      *
      * @throws Exception
      */
-    public function __construct() {
+    public function __construct()
+    {
         $this->bearer = config('gridpane-laravel.bearer');
-        if(!$this->bearer) {
+        if (! $this->bearer) {
             throw new InvalidArgumentException('Please set GP_BEARER environment variables.');
         }
         $this->client = new HttpClient();
@@ -27,9 +30,10 @@ class GridPaneService {
      *
      * @return mixed
      */
-    public function __call($method, $args) {
-        if(is_callable([$this->client,$method])) {
-            return call_user_func_array([$this->client,$method],$args);
+    public function __call($method, $args)
+    {
+        if (is_callable([$this->client, $method])) {
+            return call_user_func_array([$this->client, $method], $args);
         } else {
             throw new BadMethodCallException("Method $method does not exist");
         }
@@ -40,12 +44,12 @@ class GridPaneService {
      *
      * @return mixed
      */
-    public function __get($property) {
-        if(property_exists($this->client,$property)) {
+    public function __get($property)
+    {
+        if (property_exists($this->client, $property)) {
             return $this->client->{$property};
         } else {
             throw new BadMethodCallException("Property $property does not exist");
         }
     }
-
 }
